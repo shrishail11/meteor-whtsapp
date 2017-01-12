e50cf6fee6917f6d19ca0d369cb15a0e9d856211
Shrishail Deshpande <deshpans@brocade.com>
2017-01-13 00:38:43 +0530

    message

---
+++ client/scripts/controllers/chats.controller.js
@@ -0,0 +1,28 @@
+import Moment from 'moment';
+import { Controller } from 'angular-ecmascript/module-helpers';
+ import { Chats } from '../../../lib/collections';
+
+export default class ChatsCtrl extends Controller {
+    constructor() {
+    super(...arguments);
+ 
+     this.helpers({
+      data() {
+        return Chats.find();
+      }
+    });
+        
+  }
+    
+ showNewChatModal() {
+    this.NewChat.showModal();
+  }
+ 
+  remove(chat) {
+    this.callMethod('removeChat', chat._id);
+  }
+}
+
+ 
+ChatsCtrl.$name = 'ChatsCtrl';
+ChatsCtrl.$inject = ['NewChat'];
